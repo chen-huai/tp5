41bfda2ff4accdf623ce4f8cef068e473f4f4baa
chenhuai <457794287@qq.com>
2019-12-31 16:31:31 +0800

    thinkphp 数据库的学习

---
+++ application/index/controller/User.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+use app\index\model\User as UserModel;
+class User
+{
+    //URL访问规则：http://serverName/index.php（或者其它应用入口文件）/模块/控制器/操作/[参数名/
+    //新增数据http://www.tp.com/index/user/add
+    //首先需要user同名的数据库
+    //index模块目录,user控制器,add方法
+    //model为模型
+
+    public function add()
+    {
+        $user = new UserModel();
+        $user->name = '刘云';
+        $user->email = 'liuyun@qq.com';
+        $user->birthday = strtotime('1989-02-09');
+        if($user->save())
+        {
+            return '用户新增成功';
+        }else{
+            return '用户新增失败';
+        }
+    }
+
+}
---
+++ application/index/model/User.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\index\model;
+use think\Model;
+class User extends Model
+//user继承model类,模型还需要在控制器里创建一个跟数据库表名一样名字的控制器
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
