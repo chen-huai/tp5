c11eaffbaba9476e8a94806245f515d595762d15
chenhuai <457794287@qq.com>
2020-01-01 11:40:55 +0800

    模块化数据操作

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,8 +8,12 @@ class Index
     public function index()
     {
 
-        //return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
-        //实例化对象
+        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+
+    }
+    public function request()
+    {
+        //实例化对象http://www.tp.com/index.php/index/index/request
         $request = Request::instance();
         //整个数据集
         //dump($request).'<br />';
@@ -24,7 +28,7 @@ class Index
         echo '获取变量'.'<br />';
         //变量需要传入变量www.tp.com/?name=张三
         echo $request->param('name');
-        //获取所有变量需要传入变量www.tp.com/?name=张三&password=123456
+        //获取所有变量需要传入变量www.tp.com/index.php/index/index/request/?name=张三&password=123456
         dump($request->param());
         dump(input(''));
         dump(input('param.name'));
@@ -34,6 +38,8 @@ class Index
         //www.tp.com/index.json
         dump($request->ext());
     }
+
+
     public function db()
     {
         //数据库
--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -3,15 +3,14 @@ namespace app\index\controller;
 use app\index\model\User as UserModel;
 class User
 {
-
     //URL访问规则：http://serverName/index.php（或者其它应用入口文件）/模块/控制器/操作/[参数名/
     //新增数据http://www.tp.com/index/user/add
-    //首先需要user同名的数据库
+    //首先需要user同名的数据库,即think_user，因为配置文件已经有申明为think_前缀
     //index模块目录,user控制器,add方法
     //model为模型
-h
     public function add()
     {
+        /*
         $user = new UserModel();
         $user->name = '刘云';
         $user->email = 'liuyun@qq.com';
@@ -21,7 +20,156 @@ h
             return '用户新增成功';
         }else{
             return '用户新增失败';
+        */
+        /*
+        $user['name'] = '看云';
+        $user['email'] = 'kanyun@qq.com';
+        $user['birthday'] = strtotime('1989-02-09');
+        if($result=UserModel::create($user))
+        {
+            return '用户新增成功';
+        }else{
+            return '用户新增失败';
+           */
+        $user = new UserModel();
+        $list=[
+            ['name'=>'张三','email'=>'zhangsan@qq.com','birthday'=>strtotime('1999-5-6')],
+            ['name'=>'李四','email'=>'lisi@qq.com','birthday'=>strtotime('2000-12-25')],
+            ['name'=>'王五','email'=>'wangwu@qq.com','birthday'=>strtotime('2001-11-6')],
+        ];
+        if($user->saveAll($list))
+        {
+            return '用户新增成功';
+        }else{
+            return '用户新增失败';
         }
     }
 
+    public function update()
+    {
+        //先查找后更新http://tp.com/index/user/update
+        /*
+        $user = UserModel::get(1);
+        //dump($user);
+        $user->name = '刘云';
+        $user->email = 'liuyun@qq.com';
+        if($user->save())
+        {
+            return '用户更新成功';
+        }else{
+            return '用户更新失败';
+        }
+        */
+        //$user = UserModel::get(1);
+        //$user->save(['name'=>'刘涛','email'=>'liutao@qq.com'],['id'=>1]);
+        /*
+        //批量更新数据，实例化
+        $user = new UserModel();
+        $list = [
+            ['id'=>2,'name'=>'蒋欣','email'=>'jiangxin@qq.com'],
+            ['id'=>3,'name'=>'钟汉良','email'=>'zhonghanliang@qq.com'],
+        ];
+        $user->saveAll($list);
+        */
+        /*
+        $user = new UserModel();
+        $user->update(['id'=>4,'name'=>'郑伊健','email'=>'zhengyijina@qq.com']);
+        */
+        UserModel::update(['id'=>5,'name'=>'刘德华','email'=>'liudehua@qq.com']);
+
+    }
+    //查询数据
+    public function select()
+    {
+        /*
+        //取出主键，根据字段查询
+        $user = UserModel::get(1);
+        //dump($user);
+        echo $user->name.'<br />';
+        echo $user->email.'<br />';
+        */
+        /*
+        //用数组查询
+        $user = UserModel::get(['name'=>'蒋欣']);
+        echo $user->email;
+        */
+        /*
+        //实例化
+        $user = new UserModel();
+        $result=$user->where('name','钟汉良')->find();
+        echo $result->email;
+        */
+        //获取多个数据,id为1.2.3的数据
+        /*
+        $list=UserModel::all([1,2,3]);
+        foreach ($list as $key=>$value)
+        {
+            echo $value->name.'<br />';
+            echo $value->email.'<br />';
+        }
+        */
+        /*
+        //
+        $user = new UserModel();
+        $result = $user->where('id',2)->limit(2)->order('id','desc')->select();
+        //dump($result);
+        foreach ($result as $key=>$value)
+        {
+            echo $value['name'].'<br />';
+            echo $value['email'].'<br />';
+        }
+        */
+        //聚合函数的调用
+        $user = new UserModel();
+        echo $user->count('id').'<br />';
+        echo $user->max('birthday');
+
+    }
+
+    //删除数据
+    public function delete()
+    {
+        /*
+        $user = UserModel::get(1);
+        if ($user->delete()){
+            return '删除成功';
+        }else{
+            return '删除失败';
+        }
+        */
+        /*
+        if (UserModel::destroy(2))
+        {
+            return '删除成功';
+        }else{
+            return '删除失败';
+        }
+        */
+        //删除多条数据
+        /*
+        if (UserModel::destroy([3,4]))
+        {
+            return '删除成功';
+        }else{
+            return '删除失败';
+        }
+        */
+        //条件删除
+        /*
+        if (UserModel::destroy(['name'=>'张三']))
+        {
+            return '删除成功';
+        }else{
+            return '删除失败';
+        }
+        */
+        $result = UserModel::where('id','>',7)->delete();
+        if ($result)
+        {
+            return '删除成功';
+        }else{
+            return '删除失败';
+        }
+
+    }
 }
